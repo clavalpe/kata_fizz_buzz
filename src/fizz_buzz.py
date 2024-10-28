d91b942d680f6c86ad8578d62e27b017e44c41ef
Clara <clavalpe@upv.es>
2024-10-28 19:21:39 +0100

    refactor - extract is_buzz method

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,15 +1,18 @@
 class FizzBuzz:
     def execute(self, number):
-        if number % 3 == 0 and number % 5 == 0:
+        if self._is_fizz(number) and self._is_buzz(number):
             return "FizzBuzz"
 
         if self._is_fizz(number):
             return "Fizz"
         
-        if number % 5 == 0:
+        if self._is_buzz(number):
             return "Buzz"
         
         return str(number)
 
     def _is_fizz(self, number):
         return number % 3 == 0
+    
+    def _is_buzz(self, number):
+        return number % 5 == 0
