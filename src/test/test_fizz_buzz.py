8eea0900a6b102a87fea1a5e9d7b08c70c616812
Clara <clavalpe@upv.es>
2024-10-28 18:22:57 +0100

    red fizzbuzz

--- src/test/test_fizz_buzz.py
+++ src/test/test_fizz_buzz.py
@@ -13,3 +13,6 @@ class TestFizzBuzz:
 
     def test_it_converts_5_to_buzz(self):
         assert "Buzz" == FizzBuzz().execute(5)
+
+    def test_it_converts_15_to_fizz_buzz(self):
+        assert "FizzBuzz" == FizzBuzz().execute(15)
