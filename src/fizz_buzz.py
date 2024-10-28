bf3ba1b885cd42e8423e3b4c8813b8c6e9dcd4e9
Clara <clavalpe@upv.es>
2024-10-28 18:25:48 +0100

    implemented fizzbuzz

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,5 +1,8 @@
 class FizzBuzz:
     def execute(self, number):
+        if number % 3 == 0 and number % 5 == 0:
+            return "FizzBuzz"
+
         if number % 3 == 0:
             return "Fizz"
         
