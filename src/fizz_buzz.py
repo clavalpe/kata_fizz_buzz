a2bc3b228e39ea7bb31edecaa881c891ad88f17d
Clara <clavalpe@upv.es>
2024-10-28 18:00:01 +0100

    first commit

---
+++ src/fizz_buzz.py
@@ -0,0 +1,6 @@
+class FizzBuzz:
+    def execute(self, number):
+        if number % 3 == 0:
+            return "Fizz"
+        
+        return str(number)
---
+++ src/test/test_fizz_buzz.py
@@ -0,0 +1,15 @@
+from fizz_buzz import FizzBuzz
+
+
+class TestFizzBuzz:
+    def test_it_converts_1_to_1(self):
+        assert "1" == FizzBuzz().execute(1)
+
+    def test_it_converts_2_to_2(self):
+        assert "2" == FizzBuzz().execute(2)
+
+    def test_it_converts_3_to_fizz(self):
+        assert "Fizz" == FizzBuzz().execute(3)
+
+    def test_it_converts_5_to_buzz(self):
+        assert "Buzz" == FizzBuzz().execute(5)
